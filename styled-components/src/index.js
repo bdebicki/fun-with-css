04f8636e7c435f8cb6134980a923b581898c155f
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-14 14:51:54 +0200

    hide .less watcher + add support for .jsx filrs

--- styled-components/src/index.js
+++ styled-components/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './js/app';
 
-import './less/app.less';
+// import './less/app.less';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- styled-components/webpack.config.js
+++ styled-components/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
@@ -9,26 +9,30 @@ const config = {
     filename: 'bundle.js',
     publicPath: '/public'
   },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*','.js','.jsx']
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(jsx|js)$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       },
-      {
-        test: /\.less$/i,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader']
-        })
-      }
+      // {
+      //   test: /\.less$/i,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: ['css-loader', 'less-loader']
+      //   })
+      // }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css',
-    }),
+  //   new ExtractTextPlugin({
+  //     filename: 'style.css',
+  //   }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
