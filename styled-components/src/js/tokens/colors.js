04c6bb096d516b9c68256228519f20899621ef14
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-15 12:32:35 +0200

    extend tokens

--- styled-components/src/js/tokens/colors.js
+++ styled-components/src/js/tokens/colors.js
@@ -1,2 +1,5 @@
-export const BLUE_BASE = '#006cff';
+export const SILVER_BASE = '#f3f3f3';
 export const GRAY_BASE = '#444';
+export const BLUE_BASE = '#006cff';
+export const RED_BASE = '#e20c00';
+export const GREEN_BASE = '#63ad0e';
--- styled-components/src/js/tokens/typography.js
+++ styled-components/src/js/tokens/typography.js
@@ -3,7 +3,9 @@ import { GRAY_BASE } from './colors'
 export const FONT_FAMILY_BASE = 'Arial, sans-serif';
 
 export const FONT_SIZE_BASE = '14px';
+export const FONT_SIZE_SMALL = '12px';
 
 export const LINE_HEIGHT_BASE = '18px';
+export const LINE_HEIGHT_SMALL = '16px';
 
 export const TEXT_COLOR_BASE = GRAY_BASE;
