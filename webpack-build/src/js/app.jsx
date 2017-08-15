04059358b89c7c57f2a869a79ddebbf28268dda8
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-15 18:03:42 +0200

    add button and page styles

--- webpack-build/src/js/app.jsx
+++ webpack-build/src/js/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Button from './components/button';
 import '../less/app.less';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h5>fun with css</h5>
-        <h1>webpack build</h1>
+      <div className="reactWrapper">
+        <h5 className="title">fun with css</h5>
+        <h1 className="headline">webpack build</h1>
+        <Button>boczek</Button>
       </div>
     )
   }
