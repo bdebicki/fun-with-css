f7d0ca7322cc77c805cf4636ce4c0296e9cdd1f9
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-15 14:47:20 +0200

    initial commit for css modules

---
+++ css-modules/src/js/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h5>fun with css</h5>
+        <h1>css modules</h1>
+      </div>
+    )
+  }
+}
---
+++ css-modules/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*','.js','.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ]
+};
+
+module.exports = config;
