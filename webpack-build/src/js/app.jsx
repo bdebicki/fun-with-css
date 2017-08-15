ba1908a396ce0d2ad38aa7b9b9d4d2763c4db52f
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-15 15:21:43 +0200

    initial webpack-build

---
+++ webpack-build/src/js/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../less/app.less';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h5>fun with css</h5>
+        <h1>webpack build</h1>
+      </div>
+    )
+  }
+}
---
+++ webpack-build/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*','.js','.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/i,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ]
+};
+
+module.exports = config;
