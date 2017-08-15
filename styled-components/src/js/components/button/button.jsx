effbacadc4db15089b9dca1f92d2b8c169a4d3fe
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-15 18:49:08 +0200

    revert removing button component from styled components

---
+++ styled-components/src/js/components/button/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled, { injectGlobal } from 'styled-components';
+
+import Button from 'button-styles';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Button />
+    )
+  }
+}
