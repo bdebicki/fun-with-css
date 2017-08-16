99cc6dcf2129e2f4f0e226316ba5f7d7cd77a8c9
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-16 14:30:05 +0200

    add verison to button element

--- css-modules/src/js/components/button.jsx
+++ css-modules/src/js/components/button.jsx
@@ -5,10 +5,52 @@ import PropTypes from 'prop-types';
 import btn from '../../less/components/button.less';
 
 export default class Button extends React.Component {
+  btnWeight() {
+    if(this.props.weight === 'light') {
+        return btn.lightWeight;
+    }
+    return '';
+  }
+  btnStyle() {
+    if(this.props.style === 'neutral') {
+        return btn.neutralStyle;
+    }
+    if(this.props.style === 'success') {
+        return btn.successStyle;
+    }
+    if(this.props.style === 'danger') {
+        return btn.dangerStyle;
+    }
+    return '';
+  }
+  btnSize() {
+    if(this.props.size === 'large') {
+        return btn.largeSize;
+    }
+    if(this.props.size === 'small') {
+        return btn.smallSize;
+    }
+    return '';
+  }
+  btnStrech() {
+    if(this.props.streched) {
+        return btn.stretchedSize;
+    }
+    return false;
+  }
+
   render() {
+    const weight = this.btnWeight();
+    const style = this.btnStyle();
+    const size = this.btnSize();
+    const strech = this.btnStrech();
+
     const classes = classNames(
       btn.btn,
-      this.props.className
+      weight,
+      style,
+      size,
+      strech
     );
 
     return (
@@ -18,10 +60,18 @@ export default class Button extends React.Component {
 }
 
 Button.propTypes = {
+  children: PropTypes.string.isRequired,
   className: PropTypes.string,
-  children: PropTypes.string.isRequired
+  btnWeight: PropTypes.string,
+  btnStyle: PropTypes.string,
+  btnSize: PropTypes.string,
+  btnStreched: PropTypes.bool
 };
 
 Button.defaultProps = {
-  className: null
+  className: null,
+  btnWeight: null,
+  btnStyle: null,
+  btnSize: null,
+  btnStreched: false
 };
